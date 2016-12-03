fd2891bc4e0d8dadc694818613beb1be378254e0
LeapSpace <yukee.donghao@gmail.com>
2016-12-03 17:21:37 +0800

    bug fixed

--- wesocket/websocket.py
+++ wesocket/websocket.py
@@ -2,124 +2,108 @@
 
 import io
 import os
-from subprocess import Popen, PIPE
 from struct import Struct
+from subprocess import PIPE, Popen
 from threading import Thread
-from time import sleep, time
+from time import sleep
 from wsgiref.simple_server import make_server
 
 import picamera
-from ws4py.websocket import WebSocket
-from ws4py.server.wsgirefserver import WSGIServer, WebSocketWSGIRequestHandler
+from ws4py.server.wsgirefserver import WebSocketWSGIRequestHandler, WSGIServer
 from ws4py.server.wsgiutils import WebSocketWSGIApplication
+from ws4py.websocket import WebSocket
 
-###########################################
-# CONFIGURATION
-WIDTH = 640
-HEIGHT = 480
-FRAMERATE = 24
+
+WIDTH = 800
+HEIGHT = 608
+FRAMERATE = 30
 WS_PORT = 8084
-COLOR = u'#444'
-BGCOLOR = u'#333'
 JSMPEG_MAGIC = b'jsmp'
 JSMPEG_HEADER = Struct('>4sHH')
-###########################################
 
 
 class StreamingWebSocket(WebSocket):
-	def opened(self):
-		self.send(JSMPEG_HEADER.pack(JSMPEG_MAGIC, WIDTH, HEIGHT), binary=True)
+    def opened(self):
+        self.send(JSMPEG_HEADER.pack(JSMPEG_MAGIC, WIDTH, HEIGHT), binary=True)
 
 
 class BroadcastOutput(object):
-	def __init__(self, camera):
-		print('Spawning background conversion process')
-		self.converter = Popen([
-			'avconv',
-			'-f', 'video4linux2',
-			'-pix_fmt', 'yuv420p',
-			'-s', '%dx%d' % camera.resolution,
-			'-r', str(float(camera.framerate)),
-			'-i', '/dev/video0',
-			'-v', 'quiet',
-			'-f', 'mpeg1video',
-			'-b', '800k',
-			'-r', str(float(camera.framerate)),
-			'pipe:1'],
-			stdin=PIPE, stdout=PIPE, stderr=io.open(os.devnull, 'wb'),
-			shell=True, close_fds=True)
+    def __init__(self, camera):
+        print('Spawning background conversion process')
+        self.converter = Popen([
+            'avconv',
+            '-threads', '1',
+            '-f', 'rawvideo',
+            '-pix_fmt', 'yuv420p',
+            '-s', '%dx%d' % camera.resolution,
+            '-r', str(float(camera.framerate)),
+            '-i', '-',
+            '-f', 'mpeg1video',
+            '-b', '1000k',
+            '-r', str(float(camera.framerate)),
+            '-vf', 'hflip',
+            '-'],
+            stdin=PIPE, stdout=PIPE, stderr=io.open(os.devnull, 'wb'),
+            shell=False, close_fds=True)
 
-	def write(self, b):
-		self.converter.stdin.write(b)
+    def write(self, b):
+        self.converter.stdin.write(b)
 
-	def flush(self):
-		print('Waiting for background conversion process to exit')
-		self.converter.stdin.close()
-		self.converter.wait()
+    def flush(self):
+        print('Waiting for background conversion process to exit')
+        self.converter.stdin.close()
+        self.converter.wait()
 
 
 class BroadcastThread(Thread):
-	def __init__(self, converter, websocket_server):
-		super(BroadcastThread, self).__init__()
-		self.converter = converter
-		self.websocket_server = websocket_server
+    def __init__(self, converter, websocket_server):
+        super(BroadcastThread, self).__init__()
+        self.converter = converter
+        self.websocket_server = websocket_server
 
-	def run(self):
-		try:
-			i=0
-			while True:
-				buf = self.converter.stdout.read(512)
-				if buf:
-					self.websocket_server.manager.broadcast(buf, binary=True)
-				elif self.converter.poll() is not None:
-					break
-				else:
-					continue
-		except Exception as e:
-			raise e
-		finally:
-			self.converter.stdout.close()
+    def run(self):
+        try:
+            while True:
+                buf = self.converter.stdout.read(512)
+                if buf:
+                    self.websocket_server.manager.broadcast(buf, binary=True)
+                elif self.converter.poll() is not None:
+                    break
+        finally:
+            self.converter.stdout.close()
 
 
 def main():
-	print('Initializing camera')
-	with picamera.PiCamera() as camera:
-		camera.resolution = (WIDTH, HEIGHT)
-		camera.framerate = FRAMERATE
-		sleep(1) # camera warm-up time
-		print('Initializing websockets server on port %d' % WS_PORT)
-		websocket_server = make_server(
-			'127.0.0.1', WS_PORT,
-			server_class=WSGIServer,
-			handler_class=WebSocketWSGIRequestHandler,
-			app=WebSocketWSGIApplication(handler_cls=StreamingWebSocket))
-		websocket_server.initialize_websockets_manager()
-		websocket_thread = Thread(target=websocket_server.serve_forever)
-		output = BroadcastOutput(camera)
-		broadcast_thread = BroadcastThread(output.converter, websocket_server)
-		print('Starting recording')
-		camera.start_recording(output, 'yuv')
-		try:
-			print('Starting websockets thread')
-			websocket_thread.start()
-			print('Starting broadcast thread')
-			broadcast_thread.start()
-			while True:
-				camera.wait_recording(1)
-		except KeyboardInterrupt:
-			pass
-		except Exception as e:
-			raise e
-		finally:
-			print('Stopping recording')
-			camera.stop_recording()
-			print('Waiting for broadcast thread to finish')
-			broadcast_thread.join()
-			print('Shutting down websockets server')
-			websocket_server.shutdown()
-			print('Waiting for websockets thread to finish')
-			websocket_thread.join()
+    with picamera.PiCamera() as camera:
+        camera.resolution = (WIDTH, HEIGHT)
+        camera.framerate = FRAMERATE
+        sleep(1)  # camera warm-up time
+
+        websocket_server = make_server(
+            '0.0.0.0', WS_PORT,
+            server_class=WSGIServer,
+            handler_class=WebSocketWSGIRequestHandler,
+            app=WebSocketWSGIApplication(handler_cls=StreamingWebSocket))
+        websocket_server.initialize_websockets_manager()
+        websocket_thread = Thread(target=websocket_server.serve_forever)
+
+        output = BroadcastOutput(camera)
+        broadcast_thread = BroadcastThread(output.converter, websocket_server)
+
+        camera.start_recording(output, 'yuv')
+        try:
+            websocket_thread.start()
+            broadcast_thread.start()
+            while True:
+                camera.wait_recording(1)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            camera.stop_recording()
+            broadcast_thread.join()
+            websocket_server.shutdown()
+            websocket_thread.join()
 
 
 if __name__ == '__main__':
-	main()
+    main()
